7d24543ca97c325e94d0a5f059fe1a54fc698302
VAN BOSSUYT Nicolas <nivb@odoo.com>
2024-01-22 09:57:25 +0100

    Added clang-format.

--- ce-heap.h
+++ ce-heap.h
@@ -1,69 +1,75 @@
 #pragma once
 
-#include "ce-base.h"
+#include <ce-base.h>
+#include <ce-macros.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+CE_HEADER_BEGIN
 
-#define CE_HEAP_MAGIC 0xc0c0c0c0c0c0c0c0
-#define CE_HEAP_DEAD 0xdeaddeaddeaddead
-#define CE_HEAP_ALIGN (64)
-#define CE_HEAP_PAGE_SIZE (4096)
-#define CE_HEAP_MIN_REQU (4096 * 4)
+#define CE_HEAP_MAGIC      0xc0c0c0c0c0c0c0c0
+#define CE_HEAP_DEAD       0xdeaddeaddeaddead
+#define CE_HEAP_ALIGN      (64)
+#define CE_HEAP_PAGE_SIZE  (4096)
+#define CE_HEAP_MIN_REQU   (4096 * 4)
 #define CE_HEAP_ALIGNED(X) (((X) + (CE_HEAP_ALIGN - 1)) & ~(CE_HEAP_ALIGN - 1))
-#define CE_HEAP_PAGE_ALIGNED(X)                                                \
-  (((X) + (CE_HEAP_PAGE_SIZE - 1)) & ~(CE_HEAP_PAGE_SIZE - 1))
+#define CE_HEAP_PAGE_ALIGNED(X) \
+    (((X) + (CE_HEAP_PAGE_SIZE - 1)) & ~(CE_HEAP_PAGE_SIZE - 1))
 
 typedef struct ce_heap_node ce_heap_node;
 typedef struct ce_heap_major ce_heap_major;
 typedef struct ce_heap_minor ce_heap_minor;
 
-struct ce_heap_node {
-  union {
-    ce_u64 magic;
-    ce_u8 m[8];
-  };
-  ce_heap_node *prev;
-  ce_heap_node *next;
+struct ce_heap_node
+{
+    union
+    {
+        ce_u64 magic;
+        ce_u8 m[8];
+    };
+    ce_heap_node *prev;
+    ce_heap_node *next;
 };
 
-#define CE_HEAP_NODE(T)                                                        \
-  union {                                                                      \
-    ce_heap_node base;                                                         \
-    struct {                                                                   \
-      ce_u64 magic;                                                            \
-      T *prev;                                                                 \
-      T *next;                                                                 \
-    };                                                                         \
-  }
+#define CE_HEAP_NODE(T)    \
+    union                  \
+    {                      \
+        ce_heap_node base; \
+        struct             \
+        {                  \
+            ce_u64 magic;  \
+            T *prev;       \
+            T *next;       \
+        };                 \
+    }
 
-struct ce_heap_major {
-  CE_HEAP_NODE(ce_heap_major);
+struct ce_heap_major
+{
+    CE_HEAP_NODE(ce_heap_major);
 
-  ce_usize size;
-  ce_usize used;
-  ce_heap_minor *minor;
+    ce_usize size;
+    ce_usize used;
+    ce_heap_minor *minor;
 };
 
-struct ce_heap_minor {
-  CE_HEAP_NODE(ce_heap_minor);
+struct ce_heap_minor
+{
+    CE_HEAP_NODE(ce_heap_minor);
 
-  ce_usize size;
-  ce_usize used;
-  ce_heap_major *major;
+    ce_usize size;
+    ce_usize used;
+    ce_heap_major *major;
 };
 
 typedef void *ce_heap_alloc_fn(void *ctx, ce_usize size);
 
 typedef void ce_heap_free_fn(void *ctx, void *ptr, ce_usize size);
 
-typedef struct {
-  void *ctx;
-  ce_heap_alloc_fn *alloc;
-  ce_heap_free_fn *free;
-  ce_heap_major *root;
-  ce_heap_major *best;
+typedef struct
+{
+    void *ctx;
+    ce_heap_alloc_fn *alloc;
+    ce_heap_free_fn *free;
+    ce_heap_major *root;
+    ce_heap_major *best;
 } ce_heap;
 
 /* ---- Internal functions -------------------------------------------------- */
@@ -121,6 +127,4 @@ void *ce_heap_calloc(ce_heap *heap, ce_usize num, ce_usize size);
 
 void ce_heap_free(ce_heap *heap, void *ptr);
 
-#ifdef __cplusplus
-}
-#endif
+CE_HEADER_END
